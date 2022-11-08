4b4a47075b2c93b19b6b57150ee4f73075364313
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-11-08 16:22:54 +0000

    added file

---
+++ weekday.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2022 Lucas LeBlanc All rights reserved.
+#
+# Created by: Lucas LeBlanc
+# Created on: Nov 2022
+# ICS3U-Unit3-04.py File,
+# learning if...then...elseif...else... statements in python.
+
+
+def main():
+
+    # input and variables
+    input_number_as_string = input("Enter an number of the week: ")
+
+    # process and output
+    try:
+        input_number_as_int = int(input_number_as_string)
+        if input_number_as_int == 1:
+            print("\nThe {0:,}st day of the week is Sunday.".format(input_number_as_int))
+        elif input_number_as_int == 2:
+            print("\nThe {0:,}nd day of the week is Monday.".format(input_number_as_int))
+        elif input_number_as_int == 3:
+            print("\nThe {0:,}rd day of the week is Tuesday.".format(input_number_as_int))
+        elif input_number_as_int == 4:
+            print("\nThe {0:,}th day of the week is Wednesday.".format(input_number_as_int))
+        elif input_number_as_int == 5:
+            print("\nThe {0:,}th day of the week is Thursday.".format(input_number_as_int))
+        elif input_number_as_int == 6:
+            print("\nThe {0:,}th day of the week is Friday.".format(input_number_as_int))
+        elif input_number_as_int == 7:
+            print("\nThe {0:,}th day of the week is Saturday.".format(input_number_as_int))
+        else:
+            print("\n {} is not a number in the week.".format(input_number_as_int))
+    except ValueError:
+        print("\nThis is not an integer")
+
+    print("\n\nDone.")
+
+if __name__ == "__main__":
+    main()
